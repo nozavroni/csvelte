5fad24b66929ca1b1898dd78dfc43bcbf4d70e7e
Luke Visinoni <luke.visinoni@gmail.com>
2016-06-20 23:58:29 -0700

    Added bootstrap file so that I can autoload in my unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHPUnit Bootstrap
+ * The CLI test-runner calls this code before running its tests.
+ *
+ * @package   CSVelte
+ * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
+ * @author    Luke Visinoni <luke.visinoni@gmail.com>
+ */
+require __DIR__ . '/../vendor/autoload.php';
