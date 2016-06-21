9b5998d47e2c4687b3f1dd439b44c1d0463735f9
Luke Visinoni <luke.visinoni@gmail.com>
2016-06-21 12:24:13 -0700

    Added FileNotFound and PermissionDenied  exceptions and sample csv file

---
+++ src/CSVelte/Exception/FileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php namespace CSVelte\Exception;
+/**
+ * CSVelte\Exception\FileNotFoundException
+ * Thrown when user attempts to access/read a file that does not exist
+ *
+ * @package   CSVelte\Exception
+ * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
+ * @author    Luke Visinoni <luke.visinoni@gmail.com>
+ */
+class FileNotFoundException extends CSVelteException
+{
+
+}
---
+++ src/CSVelte/Exception/PermissionDeniedException.php
@@ -0,0 +1,13 @@
+<?php namespace CSVelte\Exception;
+/**
+ * CSVelte\Exception\PermissionDeniedException
+ * Thrown when user attempts to access/read a file they don't have permission for
+ *
+ * @package   CSVelte\Exception
+ * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
+ * @author    Luke Visinoni <luke.visinoni@gmail.com>
+ */
+class PermissionDeniedException extends CSVelteException
+{
+
+}
