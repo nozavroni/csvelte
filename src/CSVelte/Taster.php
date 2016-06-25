844dc130dd1d671209a065350f05cb5c3e4ceb6a
Luke Visinoni <luke.visinoni@gmail.com>
2016-06-24 20:49:06 -0700

    Added lickQuoteAndDelim and tests

--- src/CSVelte/Taster.php
+++ src/CSVelte/Taster.php
@@ -21,6 +21,8 @@ class Taster
     const LINE_FEED = 10;
     const CARRIAGE_RETURN = 13;
 
+    const SPACE = 32;
+
     /**
      * @var CSVelte\InputInterface
      */
@@ -35,7 +37,7 @@ class Taster
         $this->input = $input;
     }
 
-    public function taste()
+    public function lick()
     {
         return new Flavor;
     }
@@ -45,7 +47,9 @@ class Taster
         return preg_replace($pattern = '/(["\'])(?:(?=(\\\\?))\2.)*?\1/sm', $replace = '', $data);
     }
 
-    public function guessLineEndings()
+    // pulled from stackoverflow thread *tips hat to username "Harm"*
+    // @todo I probably will make this method protected when I'm done with testing...
+    public function lickLineEndings()
     {
         $str = $this->removeQuotedStrings($this->input->read(2500));
         $eols = [
@@ -54,7 +58,6 @@ class Taster
             self::EOL_TRS80   => "\r",    // 0x0D -      - Apple ][, TRS80
         ];
 
-        $key = "";
         $curCount = 0;
         $curEol = '';
         foreach($eols as $k => $eol) {
@@ -64,5 +67,41 @@ class Taster
             }
         }
         return $curEol;
+    }
+
+    /**
+     * The best way to determine quote and delimiter characters is when columns
+     * are quoted, often you can seek out a pattern of delim, quote, stuff, quote, delim
+     * but this only works if you have quoted columns. If you don't you have to
+     * determine these characters some other way...
+     * @todo make protected
+     */
+    public function lickQuoteAndDelim()
+    {
+        $data = $this->input->read(2500);
+        $patterns = array();
+        // delim can be anything but line breaks, quotes, or any type of spaces
+        $delim = '([^\r\n\w"\'' . chr(self::SPACE) . '])';
+        $patterns[] = '/' . $delim . ' ?(["\']).*?(\2)(\1)/'; // ,"something", - anything but whitespace or quotes followed by a possible space followed by a quote followed by anything followed by same quote, followed by same anything but whitespace
+        $patterns[] = '/(?:^|\n)(["\']).*?(\1)' . $delim . ' ?/'; // 'something', - beginning of line or line break, followed by quote followed by anything followed by quote followed by anything but whitespace or quotes
+        $patterns[] = '/' . $delim . ' ?(["\']).*?(\2)(?:^|\n)/'; // ,'something' - anything but whitespace or quote followed by possible space followed by quote followed by anything followed by quote, followed by end of line
+        $patterns[] = '/(?:^|\n)(["\']).*?(\2)(?:$|\n)/'; // 'something' - beginning of line followed by quote followed by anything followed by quote followed by same quote followed by end of line
+        foreach ($patterns as $pattern) {
+            if (preg_match_all($pattern, $data, $matches) && $matches) break;
+        }
+        if (!$matches) return array("", null); // couldn't guess quote or delim
+        $quotes = array_count_values($matches[2]);
+        arsort($quotes);
+        $quotes = array_flip($quotes);
+        if ($quote = array_shift($quotes)) {
+            $delims = array_count_values($matches[1]);
+            arsort($delims);
+            $delims = array_flip($delims);
+            $delim = array_shift($delims);
+        } else {
+            $quote = "";
+            $delim = null;
+        }
+        return array($quote, $delim);
     }
 }
