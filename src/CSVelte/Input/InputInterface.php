6da1638b1a98895ddfde06e8bb15392fcdae525e
Luke Visinoni <luke.visinoni@gmail.com>
2016-06-30 21:26:49 -0700

    Added some very basic functionality to Input\File

--- src/CSVelte/Input/InputInterface.php
+++ src/CSVelte/Input/InputInterface.php
@@ -10,17 +10,20 @@
 interface InputInterface
 {
     /**
-     * Class constructor
-     *
-     * @return void
-     */
-    public function __construct();
-
-    /**
      * Read in the specified amount of characters from the input source
      *
      * @param integer Amount of characters to read from input source
      * @return string The specified amount of characters read from input source
+     * @access public
      */
     public function read($chars);
+
+    /**
+     * Read a single line from input source and return it (and move pointer to )
+     * the beginning of the next line)
+     *
+     * @return string The next line from the input source
+     * @access public 
+     */
+    public function readLine();
 }
