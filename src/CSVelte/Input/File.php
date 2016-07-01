fba64ba6e49103a34571ba90eb444aeedc7341e1
Luke Visinoni <luke.visinoni@gmail.com>
2016-06-30 19:16:09 -0700

    Added File under Input namespace/directory

---
+++ src/CSVelte/Input/File.php
@@ -0,0 +1,31 @@
+<?php namespace CSVelte\Input;
+/**
+ * CSVelte\Input\File
+ * Represents a csv file
+ *
+ * @package   CSVelte
+ * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
+ * @author    Luke Visinoni <luke.visinoni@gmail.com>
+ */
+class File implements InputInterface
+{
+    public function __construct()
+    {
+
+    }
+
+    public function name()
+    {
+
+    }
+
+    public function read($chars)
+    {
+
+    }
+
+    public function readLine()
+    {
+        
+    }
+}
