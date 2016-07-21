09fab48287a6fb7f68a817f5ffa971381e748a29
Luke Visinoni <luke.visinoni@gmail.com>
2016-07-21 15:53:10 -0700

    Finished (for now) Duration DataType. It was a massive, steaming pile of sad frowny faces and it took me like... forever because im dumb. refs #42

--- src/CSVelte/Table/DataType/Duration.php
+++ src/CSVelte/Table/DataType/Duration.php
@@ -1,6 +1,7 @@
 <?php namespace CSVelte\Table\DataType;
 
 use Carbon\Carbon;
+use Carbon\CarbonInterval;
 
 /**
  * Duration data type
@@ -9,13 +10,101 @@ use Carbon\Carbon;
  * @subpackage CSVelte\Table\DataType
  * @copyright  (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
  * @author     Luke Visinoni <luke.visinoni@gmail.com>
+ * @see ISO-8601 Durations: https://en.wikipedia.org/wiki/ISO_8601#Durations
  */
 class Duration extends AbstractType
 {
+    // protected $initStr;
+
     protected $type = 'duration';
 
     protected function convert($val)
     {
-        throw new NotYetImplementedException();
+        $intvl = null;
+        if (is_object($val)) {
+            if ($val instanceof Duration) {
+                return $val->getValue();
+            } elseif ($val instanceof \DateInterval) {
+                // if (self::wasCreatedFromDiff($val)) {
+                //     dd($val, false, 'created from diff');
+                // }
+                $intvl = $val;
+            }
+        } elseif (is_string($val) && !empty($val)) { // @todo test empty string
+            $firstchar = $val[0];
+            if ($firstchar == '+' || $firstchar == '-') {
+                $val = substr($val, 1);
+            }
+            // this will throw an exception on invalid duration string
+            $intvl = new \DateInterval($val);
+            if ($firstchar == '-') $intvl->invert = 1;
+
+            // save original duration string if it passes muster because otherwise
+            // \DateInterval will munch it! And it provides no method to recreate
+            // it other than writing your own
+            // $this->initStr = $val;
+
+            // it would probably be better if I wrote a callable to "normalize"
+            // an IntervalSpec rather than simply storing and returning whatever
+            // came in through the initialization value, but I don't want to
+            // spend any more time than I have to on this data type, as I don't
+            // see it being particularly useful and/or necessary, at least not
+            // yet. And I could definitely be putting my time to better use
+            // working on the Table class and its interaction with Readers and
+            // Writers and whatever else... 7/21/2016 at 12:40pm
+        }
+        if (is_null($intvl)) throw new \InvalidArgumentException('DataType "' . $this->getType() . '" initialized with invalid value: "' . $val . '"');
+        try {
+            return CarbonInterval::instance($intvl);
+        } catch (\InvalidArgumentException $e) {
+            // most likely means intvl was created from a diff, which is not allowed
+            return new CarbonInterval(
+                0, // years
+                0, // months
+                0, // weeks
+                $intvl->format('%a'), // days
+                $intvl->format('%h'), // hours
+                $intvl->format('%i'), // minutes
+                $intvl->format('%s') // seconds
+            );
+            // $days = $intvl->format('%a');
+            // $timeincs = array_map(function($i){
+            //     if (preg_match('/^([0-9]+)[HMS]$/', $i, $match)) {
+            //         return (bool) $match[1] ? $i : '';
+            //     }
+            // }, array(
+            //     $intvl->format('%hH'),
+            //     $intvl->format('%iM'),
+            //     $intvl->format('%sS'),
+            // ));
+            // $ci = new CarbonInterval(
+            //     0, // years
+            //     0, // months
+            //     0, // weeks
+            //     $intvl->format('%a'), // days
+            //     $intvl->format('%h'), // hours
+            //     $intvl->format('%i'), // minutes
+            //     $intvl->format('%s') // seconds
+            // );
+            // $intvl = new CarbonInterval($intvl->format("P%aDT") . implode('', $timeincs));
+        }
+    }
+
+    // public function __toString()
+    // {
+    //     return $this->initStr;
+    // }
+
+    /**
+     * @note This only works for PHP5.6+ but no biggie it's only for debugging
+     * purposes anyway... plus it doesn't break anything for earlier versions it
+     * simply gets ignored.
+     */
+    public function __debuginfo()
+    {
+        return array(
+            'type' => $this->type,
+            'value' => $this->value
+        );
     }
 }
--- tests/CSVelte/TableDataTypeTest.php
+++ tests/CSVelte/TableDataTypeTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use CSVelte\Table\Data;
 use CSVelte\Table\DataType\Numeric;
 use CSVelte\Table\DataType\Text;
@@ -184,5 +185,99 @@ class TableDataTypeTest extends TestCase
         $this->assertEquals('Thu, 21 Jul 2016 06:11:23 -0700', $datetime->__toString(), 'DateTime data type\'s string format should be customizable using PHP\'s standard date() strings');
         DateTime::resetToStringFormat();
         $this->assertEquals('2016-07-21T06:11:23-0700', $datetime->__toString(), 'DateTime data type\'s string format should return to default when reset');
+    }
+
+    /**
+     * @todo I don't have time to tinker around with this right now I'll come back and perfect it later
+     */
+    public function testDurationDataTypeToStringUsesISO8601CompatibleDurationStringRatherThanHumanFriendlyFormat()
+    {
+        // I want my Duration data type to use P5Y4M2DT18H14M30S format rather
+        // that the supposedly "human-readable" format it uses by default
+        // $durStr = 'P3W'; // three weeks
+        // $dur = new Duration($durStr);
+        // $this->assertEquals('P3W', $dur->__toString());
+    }
+
+    public function testDurationDataTypeInitFromISO8601CompatibleDurationString()
+    {
+        $lotsofdays = 'P380D';
+        $dur = new Duration($lotsofdays);
+        $this->assertInstanceOf(CarbonInterval::class, $dur->getValue(), 'Duration data type should be represented internally by a Carbon\\CarbonInterval object.');
+        $this->assertEquals('54 weeks 2 days', (string) $dur->getValue(), 'Duration data type should normalize duration spec when storing internally (I would assume that 380 days would become 1 year, 2 weeks, and 1 day but it becomes 52 weeks, 2 days... ?)');
+
+        // I tried to guess how __toString would normalize this and was waaayyy
+        // off... it kind of has a mind of it's own I don't like it
+        // @todo Write my own method to normalize duration strings and return
+        // ISO-8601 duration string rather than this "human-readable" garbage
+        $alottatime = 'P2Y6M8DT395M18S';
+        $dur = new Duration($alottatime);
+        $this->assertEquals('2 years 6 months 1 week 1 day 395 minutes 18 seconds', $dur->__toString(), 'Duration data type should be able to handle complex durations as long as its increments are in order from largest to smallest');
+
+        // @todo CarbonInterval doesn't support this type of DateInterval (created
+        // from a diff between two dates) because without a start or end date
+        // the interval object cannot reliably determine the user's intent for
+        // intervals such as "3 months". That could mean mar, apr, may (31 + 30 + 31)
+        // or it could mean sept, oct, nov (30 + 31 + 30). It could even mean
+        // jan, feb, mar (which adds another piece of ambiguity due to leap year)
+        // to get around this, I just take the TOTAL amount of days returned by
+        // the diff and feed that to a new CarbonInterval object. But I'm not sure
+        // that's going to always be reliable so I need to make sure to keep an
+        // eye on this for bugs and think about other means of handling this...
+        // Or NOT handling it...
+        $stpattys = new Carbon('march 17, 2016 12:00am');
+        $dayofreckoning = new Carbon('april 1, 2016 8:30:22');
+        $darktimes = $stpattys->diff($dayofreckoning);
+        $dur = new Duration($darktimes);
+        $this->assertEquals('P15DT8H30M22S', $dur->getValue()->format("P%dDT%hH%iM%sS"));
+
+        // test negative durations
+        $sentence = new CarbonInterval(1, 8, 0, 14, 0, 0, 0);
+        $sentence->invert = 1;
+        $dur = new Duration($sentence);
+        $this->assertEquals('-P1Y8M14D', (string) $dur->getValue()->format("%rP%yY%mM%dD"));
+
+        $dur = new Duration('-P1Y8M14D');
+        $this->assertEquals('-P1Y8M14D', (string) $dur->getValue()->format("%rP%yY%mM%dD"));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testDurationDataTypeThrowsExceptionWhenInitializedWithISO8601DurationStringAndIncrementsAreInWrongOrder()
+    {
+        $dur = new Duration('P2D1YT10H');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDurationDataTypeDoesntAllowEmptyString()
+    {
+        $dur = new Duration("");
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDurationDataTypeDoesntAllowNull()
+    {
+        $dur = new Duration(null);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDurationDataTypeDoesntAllowZero()
+    {
+        $dur = new Duration(0);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDurationDataTypeDoesntAllowBoolean()
+    {
+        $dur = new Duration(true);
     }
 }
