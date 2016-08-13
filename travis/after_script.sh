90d835f0967fa5cc4435f4881a43ab520c4bcffd
Luke Visinoni <luke.visinoni@gmail.com>
2016-08-12 18:43:01 -0700

    Added before and after scripts for travis-ci Refs: #62

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
+
+# Set library info
+LIBRARY_NAME="CSVelte"
+LIBRARY_VERSION=$TRAVIS_TAG
+RELEASE_VERSION="${LIBRARY_NAME}-v${LIBRARY_VERSION}"
+
 if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
   vendor/bin/coveralls -v
   wget https://scrutinizer-ci.com/ocular.phar
   php ocular.phar code-coverage:upload --format=php-clover ./build/logs/clover.xml
 fi
 
+# Create packages directory
+PACKAGES_DIR="${HOME}/packages"
+mkdir "$PACKAGES_DIR"
+
 # I don't think the PHP version matters for API docs
 wget http://apigen.org/apigen.phar
 chmod +x apigen.phar
-apigen.phar generate -s src/ -d apidocs/ --access-levels=public --download --template-theme=bootstrap --title=CSVelte --tree
+apigen.phar generate -s src/ -d "${HOME}/apidocs/" --access-levels=public --download --template-theme=bootstrap --title=$LIBRARY_NAME --tree
+
+# Create tarball of API docs
+tar -czf "${HOME}/apidocs-${RELEASE_VERSION}.tar.gz" "$HOME/apidocs"
+
+# Set exclude dirs
+HIDDEN_FILES='*/\.*'
+
+# Create tarball of library
+$PKG="${PACKAGES_DIR}/${RELEASE_VERSION}.tar.gz"
+tar -czf . $PKG \
+    --exclude=$HIDDEN_FILES \
+    --exclude="./docs" \
+    --exclude="./tests" \
+    --exclude="./travis" \
+    --exclude="./phpunit.xml*" \
+    --exclude="./vendor"
+    --include="./vendor/**/Carbon/*"
+
+# Create zip of library
+# zip -r "${PACKAGES_DIR}/${RELEASE_VERSION}.zip" . -x $HIDDEN_FILES \
+#     "./docs" \
+#     "./tests" \
+#     "./travis" \
+#     "./phpunit.xml*" \
+#     "./vendor"
+
+# Now put tarballs somewhere...
 
 # tips hat to Pádraic Brady, Dave Marshall, Wouter, Graham Campbell for this
--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# download CSV test files and unzip if they dont already exist
+if [ ! -d "$HOME/sampledata" ]; then
+    #todo put these files under phpcsv.com/sampledata when you finally get phpcsv.com working...
+    wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
+    mkdir $HOME/sampledata
+    tar xvfz csvsampledata.tar.gz -C $HOME/sampledata
+fi
+echo 'date.timezone = "America/Los_Angeles"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
-# Nothing to do here...
+cp -R $HOME/sampledata/files tests/files
