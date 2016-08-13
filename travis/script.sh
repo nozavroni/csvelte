f6833e28e52b780e4791cc44ac061dffb143a16e
Luke Visinoni <luke.visinoni@gmail.com>
2016-08-12 19:26:04 -0700

    Still trying to get sample data files to download and unpack properly Refs: #62

--- travis/script.sh
+++ travis/script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-mkdir tests/files
-cp -R $SAMPLEDATA_PATH/files tests/files
 
 if [[ $TRAVIS_PHP_VERSION != "7.0" ]]; then
   vendor/bin/phpunit --coverage-text --coverage-clover ./build/logs/clover.xml
