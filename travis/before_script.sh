71987ea9874eea017fb50663e6559ac51bbec2fb
Luke Visinoni <luke.visinoni@gmail.com>
2016-08-12 19:32:05 -0700

    Stripped down travis build even more in an attempt to get it passing Refs: #62

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # download CSV test files and unzip if they dont already exist
-if [ ! -d $SAMPLEDATA_PATH ]; then
-    #todo put these files under phpcsv.com/sampledata when you finally get phpcsv.com working...
-    wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
-    mkdir $SAMPLEDATA_PATH
-    tar xvfz csvsampledata.tar.gz -C $SAMPLEDATA_PATH
-fi
+
+#todo put these files under phpcsv.com/sampledata when you finally get phpcsv.com working...
+wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
+tar xvfz csvsampledata.tar.gz -C tests/
+
 echo 'date.timezone = "America/Los_Angeles"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
