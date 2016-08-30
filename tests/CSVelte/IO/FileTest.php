ad7aacd633bbf03f06fdd8a9ac2fbb7715ee0674
Luke Visinoni <luke.visinoni@gmail.com>
2016-08-30 07:19:19 -0700

    Finished up changes to reader. resolves #91

--- tests/CSVelte/IO/FileTest.php
+++ tests/CSVelte/IO/FileTest.php
@@ -1,31 +1,12 @@
 <?php
 namespace CSVelteTest\IO;
-/**
- * Testing new Input/Output classes for v0.2.
- *
- * Version 0.2 intends to refactor how CSVelte deails with I/O to make it neater,
- * cleaner, more intuitive, etc. It intends to use the SplFileObject and its ilk.
- * This will test all the new shit. Will most likely replace the following:
- *
- *     * InputStringTest
- *     * InputTest
- *     * WritableTest
- *
- * It will also most likely effect many of the tests and code in the following:
- *
- *     * CSVelteTest
- *     * ReaderTest
- *     * TasterTest
- *     * TestFiles
- *     * WriterTest
- *
- * Also will likely require quite a bit of refactoring in the reader, writer,
- * and the taster. And probably the factory/facade as well.
- */
+
 use CSVelte\IO\File;
 use org\bovigo\vfs\vfsStream;
 /**
- * CSVelte\IO Tests
+ * CSVelte\IO\File Tests.
+ * This tests the new IO\File class that will be replacing CSVelte\Input\File and
+ * CSVelte\Output\File.
  *
  * @package   CSVelte Unit Tests
  * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
