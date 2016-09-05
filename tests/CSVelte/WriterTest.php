8595a72cee865419fc6ac0dccd8092fce4024531
Luke Visinoni <luke.visinoni@gmail.com>
2016-09-05 00:14:33 -0700

    Added new writer test (in new unit test style) - refs #11

---
+++ tests/CSVelte/WriterTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace CSVelteTest;
+
+use CSVelte\IO\File;
+use CSVelte\IO\Stream;
+use CSVelte\Writer;
+use CSVelte\Flavor;
+use CSVelte\Table\Row;
+
+/**
+ * CSVelte\Writer Tests.
+ * New Format for refactored tests -- see issue #11
+ *
+ * @package   CSVelte Unit Tests
+ * @copyright (c) 2016, Luke Visinoni <luke.visinoni@gmail.com>
+ * @author    Luke Visinoni <luke.visinoni@gmail.com>
+ * @todo      Move all of the tests from OldReaderTest.php into this class
+ * @coversDefaultClass CSVelte\Writer
+ */
+class WriterTest extends UnitTestCase
+{
+    
+}
