0e0d05acc35e066860bed8d4cf53ea809fdf5167
Luke Visinoni <luke.visinoni@gmail.com>
2016-09-16 15:54:43 -0700

    undid test file deletion

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # @todo Delete these lines entirely once you're sure they are no longer necessary...
-# wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
-# tar xvfz csvsampledata.tar.gz -C tests/
+# had to put this back because its causing all tests to fail without it
+wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
+tar xvfz csvsampledata.tar.gz -C tests/
 
 echo 'date.timezone = "America/Los_Angeles"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
