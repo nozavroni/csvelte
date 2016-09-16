47521e22f7265b50c050fa33be6c79294e1cf0d4
Luke Visinoni <luke.visinoni@gmail.com>
2016-09-15 22:21:06 -0700

    Removed test file download from travis build - refs #11

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# download CSV test files and unzip if they dont already exist
 
-#todo put these files under phpcsv.com/sampledata when you finally get phpcsv.com working...
-wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
-tar xvfz csvsampledata.tar.gz -C tests/
+# @todo Delete these lines entirely once you're sure they are no longer necessary...
+# wget https://s3-us-west-2.amazonaws.com/csvelte/csvsampledata.tar.gz
+# tar xvfz csvsampledata.tar.gz -C tests/
 
 echo 'date.timezone = "America/Los_Angeles"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
