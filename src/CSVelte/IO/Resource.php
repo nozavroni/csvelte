69b1ac9d6bfc5add6fba3ed897087b138bdfbd47
Luke Visinoni <luke.visinoni@gmail.com>
2016-09-18 01:38:12 -0700

    Fully docblocked IO\Resource
    Added docblock descriptions to all of ``IO\Resource`` class methods.
    refs #114

--- src/CSVelte/IO/Resource.php
+++ src/CSVelte/IO/Resource.php
@@ -150,6 +150,7 @@ class Resource
      * @param string  $mode The connection mode
      * @param boolean $lazy Whether connection should be deferred until an I/O
      *     operation is requested (such as read or write) on the attached stream
+     * @throws \CSVelte\Exception\IOException if connection fails 
      */
     public function __construct($uri, $mode = null, $lazy = null, $use_include_path = null, $context_options = null, $context_params = null)
     {
@@ -163,11 +164,23 @@ class Resource
         }
     }
 
+    /**
+     * Class destructor
+     */
     public function __destruct()
     {
-        $this->close();
+        $this->disconnect();
     }
 
+    /**
+     * Connect (open connection) to file/stream.
+     *
+     * File open is (by default) delayed until the user explicitly calls connect()
+     * or they request the resource with getResource().
+     *
+     * @return boolean True if connection was successful
+     * @throws \CSVelte\Exception\IOException if connection fails
+     */
     public function connect()
     {
         if (!$this->isConnected()) {
@@ -192,7 +205,14 @@ class Resource
         return $this->isConnected();
     }
 
-    protected function close()
+    /**
+     * Close connection.
+     *
+     * Close the connection to this stream (if open).
+     *
+     * @return boolean|null Whether close was successful, or null if already closed
+     */
+    public function disconnect()
     {
         if ($this->isConnected()) {
             return fclose($this->conn);
@@ -201,6 +221,19 @@ class Resource
         return;
     }
 
+    /**
+     * Set stream URI.
+     *
+     * Set the stream URI. Can only be set if the connection isn't open yet.
+     * If you try to set the URI on an open resource, an IOException will be thrown
+     *
+     * @param string $uri The URI for this stream resource to open
+     * @return $this
+     * @throws \InvalidArgumentException if not a valid stream uri
+     * @throws \CSVelte\Exception\IOException if stream has already been opened
+     * @todo I'm pretty sure that the parse_url function is too restrictive. It
+     *     will reject URIs that are perfectly valid.
+     */
     public function setUri($uri)
     {
         $this->assertNotConnected(__METHOD__);
@@ -215,9 +248,17 @@ class Resource
      * Set the fopen mode.
      *
      * Thank you to GitHub user "binsoul" whose AccessMode class inspired this
+     * Also thanks to the author(s) of Guzzle streams implementation, where the
+     * readwritehash idea came from. Both libraries are MIT licensed, so my
+     * merciless theft of their code is alright.
      *
      * @param string $mode A 1-3 character string determining open mode
+     * @return $this
+     * @throws \InvalidArgumentException if not a valid stream access mode
+     * @throws \CSVelte\Exception\IOException if stream has already been opened
      * @see http://php.net/manual/en/function.fopen.php
+     * @see https://github.com/binsoul/io-stream/blob/master/src/AccessMode.php
+     * @see https://raw.githubusercontent.com/guzzle/streams/master/src/Stream.php
      */
     public function setMode($mode = null)
     {
@@ -243,6 +284,15 @@ class Resource
         throw new InvalidArgumentException("{$mode} is not a valid stream access mode.");
     }
 
+    /**
+     * Set lazy flag.
+     *
+     * Set the lazy flag, which tells the class whether to defer the connection
+     * until the user specifically requests it.
+     *
+     * @param boolean|null Whether or not to "lazily" open the stream
+     * @return $this
+     */
     protected function setLazy($lazy)
     {
         if (is_null($lazy)) $lazy = true;
@@ -250,6 +300,17 @@ class Resource
         return $this;
     }
 
+    /**
+     * Set use include path flag.
+     *
+     * Sets whether or not fopen should search the include path for files. Can
+     * only be set if resource isn't open already. If called when resource is
+     * already open an exception will be thrown.
+     *
+     * @param boolean $use_include_path Whether to search include path for files
+     * @throws \CSVelte\Exception\IOException
+     * @return $this
+     */
     public function setUseIncludePath($use_include_path)
     {
         $this->assertNotConnected(__METHOD__);
@@ -257,7 +318,17 @@ class Resource
         return $this;
     }
 
-// loop over each wrapper and call setcontextoptions then rewrite setcontextoptions
+    /**
+     * Set stream context options and params.
+     *
+     * Sets arrays of stream context options and params. Check out the URI below
+     * for more on stream contexts.
+     *
+     * @param array|null $options Stream context options
+     * @param array|null $params  Stream Context params
+     * @return $this
+     * @see http://php.net/manual/en/stream.contexts.php
+     */
     public function setContext($options = null, $params = null)
     {
         if (is_array($options)) {
@@ -271,6 +342,17 @@ class Resource
         return $this;
     }
 
+    /**
+     * Retrieve underlying stream resource.
+     *
+     * An accessor method for the underlying stream resource object. Also triggers
+     * stream connection if in lazy open mode. Because this method may potentially
+     * call the connect() method, it is possible that it may throw an exception
+     * if there is some issue with opening the stream.
+     *
+     * @return resource The underlying stream resource
+     * @throws \CSVelte\Exception\IOException
+     */
     public function getResource()
     {
         if (!$this->isConnected()) {
@@ -279,16 +361,39 @@ class Resource
         return $this->conn;
     }
 
+    /**
+     * Is the stream connection open?
+     *
+     * Tells you whether this stream resource is open or not.
+     *
+     * @return boolean Whether the stream is open
+     */
     public function isConnected()
     {
         return is_resource($this->conn);
     }
 
+    /**
+     * Get the stream URI.
+     *
+     * Accessor method for stream URI.
+     *
+     * @return string The stream URI
+     */
     public function getUri()
     {
         return $this->uri;
     }
 
+    /**
+     * Get the access mode.
+     *
+     * Tells you what the access mode is. This is the short string of characters
+     * that you would pass to the fopen function to tell it how to open a file/stream
+     *
+     * @return string The file/stream access mode
+     * @see http://php.net/manual/en/function.fopen.php
+     */
     public function getMode()
     {
         return sprintf(
@@ -299,26 +404,50 @@ class Resource
         );
     }
 
+    /**
+     * Is access mode binary-safe?
+     * @return boolean Whether binary-safe flag is set
+     */
     public function isBinary()
     {
         return $this->flag == "b";
     }
 
+    /**
+     * Is stream connected in text mode?
+     * @return boolean Whether text mode flag is set
+     */
     public function isText()
     {
         return $this->flag == "t";
     }
 
+    /**
+     * Is this a lazy open resource?
+     * @return boolean Whether this is a lazily-opened resource
+     */
     public function isLazy()
     {
         return $this->lazy;
     }
 
+    /**
+     * Should fopen search include path?
+     * @return boolean Whether fopen should search include path for files
+     */
     public function getUseIncludePath()
     {
         return $this->useIncludePath;
     }
 
+    /**
+     * Update the stream context.
+     *
+     * After setting/updating stream context options and/or params, this method
+     * must be called in order to update the stream context resource.
+     *
+     * @return $this
+     */
     protected function updateContext()
     {
         // if already connected, set the options on the context resource
@@ -335,6 +464,19 @@ class Resource
         return $this;
     }
 
+    /**
+     * Set context options.
+     *
+     * Sets stream context options for this stream resource.
+     *
+     * @param array $options An array of stream context options
+     * @param string $wrapper The wrapper these options belong to (if no wrapper
+     *     argument, then $options should be an associative array with key being
+     *     a wrapper name and value being its options)
+     * @return $this
+     * @throws \InvalidArgumentException if passed invalid options or wrapper
+     * @see http://php.net/manual/en/stream.contexts.php
+     */
     public function setContextOptions($options, $wrapper = null)
     {
         if (is_array($options)) {
@@ -350,6 +492,16 @@ class Resource
         throw new InvalidArgumentException("Context options must be an array, got: " . gettype($options));
     }
 
+    /**
+     * Set context params.
+     *
+     * Set the context params for this stream resource.
+     *
+     * @param array $params An array of stream resource params
+     * @return $this
+     * @throws \InvalidArgumentException if passed invalid params
+     * @see http://php.net/manual/en/stream.contexts.php
+     */
     public function setContextParams($params)
     {
         if (is_array($params)) {
@@ -360,6 +512,16 @@ class Resource
         throw new InvalidArgumentException("Context parameters must be an array, got: " . gettype($params));
     }
 
+    /**
+     * Get context options for this stream resource.
+     *
+     * Returns the stream context options for this stream resource. Either all
+     * options for all wrappers, or just the options for the specified wrapper.
+     *
+     * @param  string $wrapper If present, return options only for this wrapper
+     * @return array Context options (either all or for specified wrapper)
+     * @throws \InvalidArgumentException if the wrapper doesn't exist
+     */
     public function getContextOptions($wrapper = null)
     {
         if (is_null($wrapper)) {
@@ -371,11 +533,22 @@ class Resource
         }
     }
 
+    /**
+     * Get context params for this stream resource.
+     *
+     * Returns the stream context params for this stream resource.
+     *
+     * @return array Context params for this stream resource
+     */
     public function getContextParams()
     {
         return $this->context['params'];
     }
 
+    /**
+     * Get stream context resource.
+     * @return resource|null The stream context resource
+     */
     public function getContext()
     {
         // if context resource hasn't been created, create one
@@ -389,16 +562,46 @@ class Resource
         return $this->crh;
     }
 
+    /**
+     * Does the access mode string indicate readability?
+     *
+     * Readable, in this context, only refers to the manner in which this stream
+     * resource was opened (if it even is opened yet). It is no indicator about
+     * whether or not the underlying stream actually supports read operations.
+     * It simply refers to the access mode string passed to it by the user.
+     *
+     * @return boolean Whether access mode indicates readability
+     */
     public function isReadable()
     {
         return $this->readable;
     }
 
+    /**
+     * Does the access mode string indicate writability?
+     *
+     * Writable, in this context, only refers to the manner in which this stream
+     * resource was opened (if it even is opened yet). It is no indicator about
+     * whether or not the underlying stream actually supports write operations.
+     * It simply refers to the access mode string passed to it by the user.
+     *
+     * @return boolean Whether access mode indicates writability
+     */
     public function isWritable()
     {
         return $this->writable;
     }
 
+    /**
+     * Assert that stream resource is not open.
+     *
+     * Used internally to ensure that stream is not open, since some methods should
+     * only be called on unopened stream resources.
+     *
+     * @param  string The method that is asserting
+     * @return void
+     * @throws \CSVelte\Exception\IOException if stream is open
+     */
     protected function assertNotConnected($method)
     {
         if ($this->isConnected()) {
@@ -406,10 +609,19 @@ class Resource
         }
     }
 
+    /**
+     * Assert that given wrapper is a valid, registered stream wrapper
+     *
+     * Used internally to ensure that a given stream wrapper is valid and available
+     *
+     * @param  string The name of the stream wrapper
+     * @return void
+     * @throws \InvalidArgumentException if wrapper doesn't exist
+     */
     protected function assertValidWrapper($name)
     {
         if (!in_array($name, stream_get_wrappers())) {
-            throw new InvalidArgumentException("{$name} is not a registered stream wrapper.");
+            throw new InvalidArgumentException("{$name} is not a known stream wrapper.");
         }
     }
 
