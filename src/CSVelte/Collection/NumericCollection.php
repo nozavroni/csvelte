0abc15b4aae53ffce7393e4d485bfb54164c2b11
Luke Visinoni <luke.visinoni@gmail.com>
2016-11-15 00:07:21 -0800

    Added several NumericCollection methods such as counts()

--- src/CSVelte/Collection/NumericCollection.php
+++ src/CSVelte/Collection/NumericCollection.php
@@ -15,6 +15,13 @@ namespace CSVelte\Collection;
 
 use function CSVelte\is_traversable;
 
+/**
+ * Class NumericCollection
+ *
+ * @package CSVelte\Collection
+ * @todo $this->set('foo', 'bar'); should throw an exception because only
+ *     numeric values are allowed. Either that or converted to int.
+ */
 class NumericCollection extends AbstractCollection
 {
     protected function isConsistentDataStructure($data)
@@ -76,6 +83,95 @@ class NumericCollection extends AbstractCollection
         }
         $this->set($key, $val);
         return $this;
+    }
+
+    /**
+     * Get the sum.
+     *
+     * @return mixed The sum of all values in collection
+     */
+    public function sum()
+    {
+        return array_sum($this->toArray());
+    }
+
+    /**
+     * Get the average.
+     *
+     * @return float|int The average value from the collection
+     */
+    public function average()
+    {
+        return $this->sum() / $this->count();
+    }
+
+    /**
+     * Get the mode.
+     *
+     * @return float|int The mode
+     */
+    public function mode()
+    {
+        $counts = $this->counts()->toArray();
+        arsort($counts);
+        $mode = key($counts);
+        return (strpos($mode, '.')) ? floatval($mode) : intval($mode);
+    }
+
+    /**
+     * Get the median value.
+     *
+     * @return float|int The median value
+     */
+    public function median()
+    {
+        $count = $this->count();
+        $data = $this->toArray();
+        natcasesort($data);
+        $middle = $count / 2;
+        $values = array_values($data);
+        if ($count % 2 == 0) {
+            // even number, use middle
+            $low = $values[$middle - 1];
+            $high = $values[$middle];
+            return ($low + $high) / 2;
+        }
+        // odd number return median
+        return $values[$middle];
+    }
+
+    /**
+     * Get the maximum value.
+     *
+     * @return mixed The maximum
+     */
+    public function max()
+    {
+        return max($this->data);
+    }
+
+    /**
+     * Get the minimum value.
+     *
+     * @return mixed The minimum
+     */
+    public function min()
+    {
+        return min($this->data);
+    }
+
+    /**
+     * Get the number of times each item occurs in the collection.
+     *
+     * This method will return a NumericCollection where keys are the
+     * values and values are the number of times that value occurs in
+     * the original collection.
+     *
+     * @return Collection
+     */
+    public function counts()
+    {
+        return static::factory(array_count_values($this->toArray()));
     }
 
 }
--- tests/CSVelte/Collection/NumericCollectionTest.php
+++ tests/CSVelte/Collection/NumericCollectionTest.php
@@ -41,4 +41,67 @@ class NumericCollectionTest extends UnitTestCase
         $coll->decrement($zero);
         $this->assertEquals(-888, $coll->get($zero));
     }
+
+
+    public function testSumMethodSumsCollection()
+    {
+        $coll = Collection::factory([10,20,30,100,60,80]);
+        $this->assertEquals(300, $coll->sum());
+    }
+
+    public function testAverageMethodAveragesCollection()
+    {
+        $coll = Collection::factory([10,20,30,100,60,80]);
+        $this->assertEquals(50, $coll->average());
+    }
+
+    public function testModeMethodReturnsCollectionMode()
+    {
+        $coll = Collection::factory([10,20,30,100,60,80,10,20,100,10,50,40,10,20,50,60,80]);
+        $this->assertEquals(10, $coll->mode());
+    }
+
+    public function testMedianMethodReturnsCollectionMedian()
+    {
+        $coll = Collection::factory([1,10,20,30,100,60,80,10,20,100,10,50,40,10,20,50,60,80]);
+        $this->assertEquals(35, $coll->median());
+
+        $coll = Collection::factory([1,20,300,4000]);
+        $this->assertEquals(160, $coll->median());
+
+        // $coll = Collection::factory(['one','two','three','four','five']);
+        // $this->assertEquals('four', $coll->median());
+
+        // @todo Maybe for strings median should work with string length?
+        // $coll = Collection::factory(['hello','world','this','will','do','weird','stuff','yes','it','will']);
+        // $this->assertEquals(0, $coll->median());
+
+        $coll = Collection::factory([1]);
+        $this->assertEquals(1, $coll->median());
+
+        $coll = Collection::factory([1,2]);
+        $this->assertEquals(1.5, $coll->median());
+    }
+
+    public function testCountsReturnsCollectionOfCounts()
+    {
+        $data = [1,1,1,2,0,2,2,3,3,3,3,3,3,3,4,5,6,6,7,8,9,0];
+        $coll = Collection::factory($data);
+        $this->assertInstanceOf(NumericCollection::class, $coll);
+        $counts = $coll->counts();
+        $this->assertInstanceOf(NumericCollection::class, $counts);
+        $this->assertEquals([
+            1 => 3,
+            2 => 3,
+            3 => 7,
+            4 => 1,
+            5 => 1,
+            6 => 2,
+            7 => 1,
+            8 => 1,
+            9 => 1,
+            0 => 2
+        ], $counts->toArray());
+    }
+
 }
