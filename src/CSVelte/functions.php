996c837f85df958924c0639dcd2a062d3bd25439
Luke Visinoni <luke.visinoni@gmail.com>
2018-07-08 15:43:21 -0700

    Added remaining IO classes necessary to properly read/write data from streams as well as their unit tests (all passing)

--- src/CSVelte/functions.php
+++ src/CSVelte/functions.php
@@ -11,3 +11,64 @@
  * @license   See LICENSE file (MIT license)
  */
 namespace CSVelte;
+
+use Iterator;
+use InvalidArgumentException;
+use CSVelte\Contract\Streamable;
+use CSVelte\IO\Stream;
+use CSVelte\IO\StreamResource;
+use CSVelte\IO\IteratorStream;
+
+/**
+ * Stream - streams various types of values and objects.
+ *
+ * You can pass a string, or an iterator, or an object with a __toString()
+ * method to this function and it will find the best possible way to stream the
+ * data from that object.
+ *
+ * @param mixed $obj The item you want to stream
+ *
+ * @throws InvalidArgumentException
+ *
+ * @return Streamable
+ *
+ * @since v0.2.1
+ */
+function streamize($obj = '')
+{
+    if ($obj instanceof Streamable) {
+        return $obj;
+    }
+
+    if ($obj instanceof StreamResource) {
+        return $obj();
+    }
+
+    if (is_resource($obj) && get_resource_type($obj) == 'stream') {
+        return new Stream(new StreamResource($obj));
+    }
+
+    if ($obj instanceof Iterator) {
+        return new IteratorStream($obj);
+    }
+
+    if (is_object($obj) && method_exists($obj, '__toString')) {
+        $obj = (string) $obj;
+    }
+    if (is_string($obj)) {
+        $stream = Stream::open('php://temp', 'r+');
+        if ($obj !== '') {
+            $res = $stream->getResource();
+            fwrite($res->getHandle(), $obj);
+            fseek($res->getHandle(), 0);
+        }
+
+        return $stream;
+    }
+
+    throw new InvalidArgumentException(sprintf(
+        'Invalid argument type for %s: %s',
+        __FUNCTION__,
+        gettype($obj)
+    ));
+}
