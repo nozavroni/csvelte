c260b4b510cc8acceeabc1b27f599f0b03c1c6a7
Luke Visinoni <luke.visinoni@gmail.com>
2018-07-12 18:42:48 -0700

    Committing my WIP before trying something else with the guessDelimByDistribution method

--- src/CSVelte/Sniffer.php
+++ src/CSVelte/Sniffer.php
@@ -13,17 +13,59 @@
 namespace CSVelte;
 
 use CSVelte\Contract\Streamable;
-use CSVelte\Dialect;
+
+use CSVelte\Exception\SnifferException;
+use Noz\Collection\Collection;
+use function Noz\to_array;
+use RuntimeException;
 
 use function Noz\collect;
 use function Stringy\create as s;
 
 class Sniffer
 {
+    /** CSV data sample size - sniffer will use this many bytes to make its determinations */
+    const SAMPLE_SIZE = 2500;
+    const DISTRIB_SECTIONS = 10;
+
+    /**
+     * End-of-line constants
+     */
+    const EOL_WINDOWS = 0;
+    const EOL_UNIX    = 1;
+    const EOL_OTHER   = 2;
+
+    /**
+     * ASCII character codes for "invisibles".
+     */
+    const HORIZONTAL_TAB  = 9;
+    const LINE_FEED       = 10;
+    const CARRIAGE_RETURN = 13;
+    const SPACE           = 32;
+
+    /**
+     * Placeholder strings -- hold the place of newlines and delimiters contained
+     * within quoted text so that the explode method doesn't split incorrectly.
+     */
+    const PLACEHOLDER_NEWLINE = '[__NEWLINE__]';
+    const PLACEHOLDER_DELIM   = '[__DELIMIT__]';
+
+    /**
+     * @var array A list of possible delimiters to check for (in order of preference)
+     */
     protected $delims = [',', "\t", ';', '|', ':', '-', '_', '#', '/', '\\', '$', '+', '=', '&', '@'];
 
+    /**
+     * @var Streamable A stream of the sample data
+     */
     protected $stream;
 
+    /**
+     * Sniffer constructor.
+     *
+     * @param Streamable $stream The data to sniff
+     * @param array $delims A list of possible delimiter characters in order of preference
+     */
     public function __construct(Streamable $stream, $delims = null)
     {
         $this->stream = $stream;
@@ -32,7 +74,14 @@ class Sniffer
         }
     }
 
-    public function setPossibleDelimiters($delims)
+    /**
+     * Set possible delimiter characters
+     *
+     * @param array $delims A list of possible delimiter characters
+     *
+     * @return self
+     */
+    public function setPossibleDelimiters(array $delims)
     {
         $this->delims = collect($delims)
             ->filter(function($val) {
@@ -40,8 +89,15 @@ class Sniffer
             })
             ->values()
             ->toArray();
+
+        return $this;
     }
 
+    /**
+     * Get list of possible delimiter characters
+     *
+     * @return array
+     */
     public function getPossibleDelimiters()
     {
         return $this->delims;
@@ -54,10 +110,274 @@ class Sniffer
      * file is formatted. This method inspects CSV data and returns its "dialect", an object that can be passed to
      * either a `CSVelte\Reader` or `CSVelte\Writer` object to tell it what "dialect" of CSV to use.
      *
+     * @todo look into which other Dialect attributes you can sniff for
+     *
      * @return Dialect
      */
     public function sniff()
     {
+        $sample = $this->stream->read(static::SAMPLE_SIZE);
+        $lineTerminator = $this->sniffLineTerminator($sample);
+        try {
+            list($quoteChar, $delimiter) = $this->sniffQuoteAndDelim($sample, $lineTerminator);
+        } catch (SnifferException $e) {
+            if ($e->getCode() !== SnifferException::ERR_QUOTE_AND_DELIM) {
+                throw $e;
+            }
+            $quoteChar = '"';
+            $delimiter = $this->sniffDelimiter($sample, $lineTerminator);
+        }
+        /**
+         * @todo Should this be null? Because doubleQuote = true means this = null
+         */
+        $escapeChar = '\\';
+        $quoteStyle = $this->sniffQuotingStyle($delimiter, $lineTerminator);
+        $header     = $this->sniffHeader($delimiter, $lineTerminator);
+        $encoding   = s($sample)->getEncoding();
 
+        return new Dialect(compact('quoteChar', 'escapeChar', 'delimiter', 'lineTerminator', 'quoteStyle', 'header', 'encoding'));
+    }
+
+    /**
+     * Sniff sample data for line terminator character
+     *
+     * @param string $data The sample data
+     *
+     * @return string
+     */
+    protected function sniffLineTerminator($data)
+    {
+        // in this case we really only care about newlines so we pass in a comma as the delim
+        $str = $this->replaceQuotedSpecialChars($data, ',');
+        $eols = [
+            static::EOL_WINDOWS => "\r\n",  // 0x0D - 0x0A - Windows, DOS OS/2
+            static::EOL_UNIX    => "\n",    // 0x0A -      - Unix, OSX
+            static::EOL_OTHER   => "\r",    // 0x0D -      - Other
+        ];
+
+        $curCount = 0;
+        $curEol = PHP_EOL;
+        foreach ($eols as $k => $eol) {
+            if (($count = substr_count($str, $eol)) > $curCount) {
+                $curCount = $count;
+                $curEol   = $eol;
+            }
+        }
+        return $curEol;
+    }
+
+    /**
+     * Sniff quote and delimiter chars
+     *
+     * The best way to determine quote and delimiter characters is when columns
+     * are quoted, often you can seek out a pattern of delim, quote, stuff, quote, delim
+     * but this only works if you have quoted columns. If you don't you have to
+     * determine these characters some other way... (see lickDelimiter).
+     *
+     * @throws SnifferException
+     *
+     * @param string $data The data to analyze
+     * @param string $lineTerminator The line terminator char/sequence
+     *
+     * @return array A two-row array containing quotechar, delimchar
+     *
+     * @todo This should throw an exception if it cannot determine the delimiter this way.
+     * @todo This should check for any line endings not just \n
+     */
+    protected function sniffQuoteAndDelim($data, $lineTerminator)
+    {
+        /**
+         * @var array An array of pattern matches
+         */
+        $matches = null;
+        /**
+         * @var array An array of patterns (regex)
+         */
+        $patterns = [];
+        // delim can be anything but line breaks, quotes, alphanumeric, underscore, backslash, or any type of spaces
+        $antidelims = implode(["\r", "\n", "\w", preg_quote('"', '/'), preg_quote("'", '/'), preg_quote(chr(self::SPACE), '/')]);
+        $delim      = "(?P<delim>[^{$antidelims}])";
+        $quote      = "(?P<quoteChar>\"|'|`)"; // @todo I think MS Excel uses some strange encoding for fancy open/close quotes
+        $patterns[] = "/{$delim} ?{$quote}.*?\2\1/ms"; // ,"something", - anything but whitespace or quotes followed by a possible space followed by a quote followed by anything followed by same quote, followed by same anything but whitespace
+        $patterns[] = "/(?:^|{$lineTerminator}){$quote}.*?\1{$delim} ?/ms"; // 'something', - beginning of line or line break, followed by quote followed by anything followed by quote followed by anything but whitespace or quotes
+        $patterns[] = "/{$delim} ?{$quote}.*?\2(?:^|{$lineTerminator})/ms"; // ,'something' - anything but whitespace or quote followed by possible space followed by quote followed by anything followed by quote, followed by end of line
+        $patterns[] = "/(?:^|{$lineTerminator}){$quote}.*?\2(?:$|{$lineTerminator})/ms"; // 'something' - beginning of line followed by quote followed by anything followed by quote followed by same quote followed by end of line
+        foreach ($patterns as $pattern) {
+            // @todo I had to add the error suppression char here because it was
+            //     causing undefined offset errors with certain data sets. strange...
+            if (@preg_match_all($pattern, $data, $matches) && $matches) {
+                break;
+            }
+        }
+        if ($matches) {
+            $qcad = collect($matches)->kintersect(array_flip(['quoteChar', 'delim']));
+            try {
+                return $qcad->map(function($val) {
+                    return collect($val)->frequency()->sort()->reverse()->getKeyAt(1);
+                    })
+                    ->ksort()
+                    ->reverse()
+                    ->values()
+                    ->toArray();
+            } catch (RuntimeException $e) {
+                // eat this exception and let the taster exception below be thrown instead...
+            }
+        }
+        throw new SnifferException('quoteChar and delimiter cannot be determined', SnifferException::ERR_QUOTE_AND_DELIM);
+    }
+
+    /**
+     * @todo To make this class more oop and test-friendly, implement strategy pattern here with each delim sniffing method implemented in its own strategy class.
+     */
+    protected function sniffDelimiter($data, $eol)
+    {
+        // build a table of characters and their frequencies for each line. We
+        // will use this frequency table to then build a table of frequencies of
+        // each frequency (in 10 lines, "tab" occurred 5 times on 7 of those
+        // lines, 6 times on 2 lines, and 7 times on 1 line)
+        // @todo it would probably make for more consistent results if you popped the last line since it will most likely be truncated due to the arbitrary nature of the sample size
+        $lines = collect(explode($eol, $this->removeQuotedStrings($data)));
+        $frequencies = $lines->map(function($line) use ($eol) {
+            $preferred = array_flip($this->getPossibleDelimiters());
+            return collect($preferred)
+                ->map(function() { return 0; })
+                ->merge(collect(s($line)->chars())->frequency()->kintersect($preferred))
+                ->toArray();
+        });
+
+        // now determine the mode for each char to decide the "expected" amount
+        // of times a char (possible delim) will occur on each line...
+        $modes = collect($this->getPossibleDelimiters())
+            ->flip()
+            ->map(function($freq, $delim) use ($frequencies) {
+                return $frequencies->getColumn($delim)->mode();
+            })
+            ->filter();
+
+        /** @var Collection $consistencies */
+        $consistencies = $frequencies->fold(function(Collection $accum, $freq, $line_no) use ($modes) {
+
+            $modes->each(function($expected, $char) use ($accum, $freq) {
+                /** @var Collection $freq */
+                if (collect($freq)->get($char) == $expected) {
+                    $matches = $accum->get($char, 0);
+                    $accum->set($char, ++$matches);
+                }
+            });
+            return $accum;
+
+        }, new Collection)
+            ->sort()
+            ->reverse();
+
+        $winners = $consistencies->filter(function($freq) use ($consistencies) {
+            return $freq === $consistencies->max();
+        });
+
+        if ($winners->count() == 1) {
+            return $consistencies->getKeyAt(1);
+        }
+
+        /**
+         * @todo Add a method here to figure out where duplicate best-match
+         *     delimiter(s) fall within each line and then, depending on
+         *     which one has the best distribution, return that one.
+         */
+        $decision = $winners->keys()->toArray();
+        try {
+            return $this->guessDelimByDistribution($data, $decision, $eol);
+        } catch (SnifferException $e) {
+            // if we still can't come to a decision, just return the first one on the preferred list
+
+        }
+    }
+
+    public function guessDelimByDistribution($data, $delims, $eol)
+    {
+        $lines = collect(explode($eol, $this->removeQuotedStrings($data)));
+        $distrib = $lines->filter(function($line) {
+                return s($line)->length() > 1;
+            })
+            ->map(function($line, $line_no) use ($delims) {
+                $line = s($line);
+                $len = $line->length();
+                $sections = collect($line->getIterator())->split(static::DISTRIB_SECTIONS);
+                return $sections->map(function($section) use ($delims) {
+                    return collect($section)->intersect($delims)->frequency()->map(function($freq, $char) use ($section) {
+                        // return what percentage of this "section" is the proposed delim char
+                        return round($freq / count($section) * 100);
+                    });
+                })
+                // fill in empty values with 0's
+                ->map(function($dist) use ($delims) {
+                    return collect($delims)->flip()->map(function($val, $char) use ($dist) { return $dist->has($char) ? $dist->get($char) : 0; })->toArray();
+                })
+                ->toArray();
+            });
+
+        // work out standard deviation
+        $destrib = $distrib->fold(function($accum, $line, $line_no) use ($delims) {
+            foreach ($delims as $delim) {
+                $accum[$delim][] = collect($line)->map(function ($section, $key) use ($delim) {
+                    return $section[$delim];
+                })->toArray();
+            }
+            return $accum;
+        }, []);
+
+        return $delims[0];
+    }
+
+    protected function sniffQuotingStyle($delimiter, $eols)
+    {
+        return Dialect::QUOTE_MINIMAL;
+    }
+
+    protected function sniffHeader($delimiter, $eols)
+    {
+        return true;
+    }
+
+    /**
+     * Replaces all quoted columns with a blank string. I was using this method
+     * to prevent explode() from incorrectly splitting at delimiters and newlines
+     * within quotes when parsing a file. But this was before I wrote the
+     * replaceQuotedSpecialChars method which (at least to me) makes more sense.
+     *
+     * @param string $data The string to replace quoted strings within
+     *
+     * @return string The input string with quoted strings removed
+     */
+    protected function removeQuotedStrings($data)
+    {
+        return preg_replace($pattern = '/(["\'])(?:(?=(\\\\?))\2.)*?\1/sm', $replace = '', $data);
+    }
+
+    /**
+     * Replace all instances of newlines and whatever character you specify (as
+     * the delimiter) that are contained within quoted text. The replacements are
+     * simply a special placeholder string. This is done so that I can use the
+     * very unsmart "explode" function and not have to worry about it exploding
+     * on delimiters or newlines within quotes. Once I have exploded, I typically
+     * sub back in the real characters before doing anything else. Although
+     * currently there is no dedicated method for doing so I just use str_replace.
+     *
+     * @param string $data  The string to do the replacements on
+     * @param string $delim The delimiter character to replace
+     *
+     * @return string The data with replacements performed
+     */
+    protected function replaceQuotedSpecialChars($data, $delim = null, $eol = null)
+    {
+        if (is_null($eol)) {
+            $eol = "\r\n|\r|\n";
+        }
+        return preg_replace_callback('/([\'"])(.*)\1/imsU', function ($matches) use ($delim, $eol) {
+            $ret = preg_replace("/({$eol})/", self::PLACEHOLDER_NEWLINE, $matches[0]);
+            if (!is_null($delim)) {
+                $ret = str_replace($delim, self::PLACEHOLDER_DELIM, $ret);
+            }
+            return $ret;
+        }, $data);
     }
 }
